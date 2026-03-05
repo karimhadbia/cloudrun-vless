4c0cb75c683bc654fff583029005f8b4124d5563
karimhadbia <146626591+karimhadbia@users.noreply.github.com>
2026-03-05 23:44:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import http from "http";
+import { WebSocketServer } from "ws";
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.end("Cloud Run Ready");
+  res.end("Cloud Run WebSocket Ready");
 });
 
-server.listen(port);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (msg) => {
+    console.log("Received:", msg.toString());
+    ws.send("OK");
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server running on port", port);
+});
